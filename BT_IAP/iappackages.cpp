6cb43bf40d4b4cc28b51b5ca3d4977811d3031f3
Dell-px <123456dell@qq.com>
2019-11-13 16:16:59 +0800

    add a checkbox to select whther the data is packed to send

---
+++ BT_IAP/iappackages.cpp
@@ -0,0 +1,6 @@
+#include "iappackages.h"
+
+IapPackages::IapPackages()
+{
+
+}
---
+++ BT_IAP/iappackages.h
@@ -0,0 +1,11 @@
+#ifndef IAPPACKAGES_H
+#define IAPPACKAGES_H
+
+
+class IapPackages
+{
+public:
+    IapPackages();
+};
+
+#endif // IAPPACKAGES_H
